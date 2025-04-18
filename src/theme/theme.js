306ef6a5fabf4bd31990c822b4d1bd1b4b49920a
Ritesh-Chandra-Roy <rcroy7india@gmail.com>
2025-04-19 03:06:06 +0530

    added mui components
    replaced generic tags with mui components on the cart page

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -13,6 +13,20 @@ const getDesignTokens = (mode) => ({
     },
     typography: {
         fontFamily: `'Roboto', 'Helvetica', 'Arial', sans-serif`,
+        defaultProps: {
+            variantMapping: {
+                h1: 'h2',
+                h2: 'h2',
+                h3: 'h2',
+                h4: 'h2',
+                h5: 'h2',
+                h6: 'h2',
+                subtitle1: 'h2',
+                subtitle2: 'h2',
+                body1: 'span',
+                body2: 'span',
+            },
+        },
     },
     components: {
         MuiButton: {
